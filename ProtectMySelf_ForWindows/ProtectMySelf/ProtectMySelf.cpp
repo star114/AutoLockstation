91d8d2dea36b76029d9d0c49199cf74839395f17
star114 <sinheesik@gmail.com>
2014-08-14 14:10:11 +0900

    fixes #5 #4

--- ProtectMySelf_ForWindows/ProtectMySelf/ProtectMySelf.cpp
+++ ProtectMySelf_ForWindows/ProtectMySelf/ProtectMySelf.cpp
@@ -4,33 +4,40 @@
 #include "stdafx.h"
 #include "ProtectMySelf.h"
 
-#pragma comment(lib,"wtsapi32.lib")
-#define MAX_LOADSTRING 100
 #define IDC_TIMER 999
 
+#define DEFAULT_INTERVAL 50*60*1000
 
 #define TRAYICONID	1//				ID number for the Notify Icon
-#define SWM_TRAYMSG	WM_APP//		the message ID sent to our window
+#define PMS_TRAYMSG	WM_APP//		the message ID sent to our window
 
-#define SWM_SHOW	WM_APP + 1//	show the window
-#define SWM_HIDE	WM_APP + 2//	hide the window
-#define SWM_EXIT	WM_APP + 3//	close the window
+#define PMS_SHOW	WM_APP + 1//	show the window
+#define PMS_HIDE	WM_APP + 2//	hide the window
+#define PMS_EXIT	WM_APP + 3//	close the window
 
-// 전역 변수:
-HINSTANCE hInst;								// 현재 인스턴스입니다.
-TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
-TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
-BOOL fBlocked;
-NOTIFYICONDATA	niData;	// notify icon data
+#define PMS_DISABLE WM_APP + 10 // disable autolock
+#define PMS_ENABLE	WM_APP + 11 // enable autolock
 
-// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
-ATOM				MyRegisterClass(HINSTANCE hInstance);
+// Global Variables:
+HINSTANCE g_hInst;								
+BOOL g_fBlocked;
+BOOL g_fDisabled;
+UINT g_nInterval;
+NOTIFYICONDATA	g_niData;	// notify icon data
+
+// Function Declaration
 BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+BOOL				OnInitDialog(HWND hWnd);
+INT_PTR CALLBACK	DlgProc(HWND, UINT, WPARAM, LPARAM);
 
 void				ShowContextMenu(HWND hWnd);
 ULONGLONG			GetDllVersion(LPCTSTR lpszDllName);
+void				SetTimer(HWND, UINT);
+void				KillTimer(HWND);
+void				Disable(HWND hWnd);
+void				Enable(HWND hWnd);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -40,30 +47,21 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// TODO: 여기에 코드를 입력합니다.
 	MSG msg;
 	HACCEL hAccelTable;
-
-	// 전역 문자열을 초기화합니다.
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_PROTECTMYSELF, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
-
-	// 응용 프로그램 초기화를 수행합니다.
-	if (!InitInstance (hInstance, nCmdShow))
-	{
+	// Initailze Application
+	if (!InitInstance (hInstance, nCmdShow)) 
 		return FALSE;
-	}
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROTECTMYSELF));
+	hAccelTable = ::LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_PROTECTMYSELF));
 
-	// 기본 메시지 루프입니다.
-	while (GetMessage(&msg, NULL, 0, 0))
+	// Massage Loop
+	while (::GetMessage(&msg, NULL, 0, 0))
 	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		if (!::TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
 		}
 	}
 
@@ -71,45 +69,10 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 }
 
 
-
 //
-//  함수: MyRegisterClass()
+//   Function: InitInstance(HINSTANCE, int)
 //
-//  목적: 창 클래스를 등록합니다.
-//
-//  설명:
-//
-//    Windows 95에서 추가된 'RegisterClassEx' 함수보다 먼저
-//    해당 코드가 Win32 시스템과 호환되도록
-//    하려는 경우에만 이 함수를 사용합니다. 이 함수를 호출해야
-//    해당 응용 프로그램에 연결된
-//    '올바른 형식의' 작은 아이콘을 가져올 수 있습니다.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_PROTECTMYSELF));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_PROTECTMYSELF);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-	return RegisterClassEx(&wcex);
-}
-
-//
-//   함수: InitInstance(HINSTANCE, int)
-//
-//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//   Purpose: Save the instance handle, and Create a main window.
 //
 //   설명:
 //
@@ -118,12 +81,15 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
+	// prepare for XP style controls
+	//InitCommonControls();
+
 	HWND hWnd;
 
-	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-	hWnd = CreateWindowW(szWindowClass, szTitle, WS_EX_TOOLWINDOW | WS_SYSMENU | WS_CAPTION,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
+	// CreateDialog
+	g_hInst = hInstance;
+	hWnd = ::CreateDialogW( hInstance, MAKEINTRESOURCE(IDD_MAIN),
+		NULL, (DLGPROC)DlgProc );
 	if (!hWnd)
 	{
 		return FALSE;
@@ -134,7 +100,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	// zero the structure - note:	Some Windows funtions require this but
 	//								I can't be bothered which ones do and
 	//								which ones don't.
-	ZeroMemory(&niData,sizeof(NOTIFYICONDATA));
+	ZeroMemory(&g_niData,sizeof(NOTIFYICONDATA));
 
 	// get Shell32 version number and set the size of the structure
 	//		note:	the MSDN documentation about this is a little
@@ -142,163 +108,156 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	//				bellow is correct
 	ULONGLONG ullVersion = GetDllVersion(_T("Shell32.dll"));
 	if(ullVersion >= MAKEDLLVERULL(5, 0,0,0))
-		niData.cbSize = sizeof(NOTIFYICONDATA);
-	else niData.cbSize = NOTIFYICONDATA_V2_SIZE;
+		g_niData.cbSize = sizeof(NOTIFYICONDATA);
+	else g_niData.cbSize = NOTIFYICONDATA_V2_SIZE;
 
 	// the ID number can be anything you choose
-	niData.uID = TRAYICONID;
+	g_niData.uID = TRAYICONID;
 
 	// state which structure members are valid
-	niData.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+	g_niData.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
 
 	// load the icon
-	niData.hIcon = (HICON)LoadImage(hInstance,MAKEINTRESOURCE(IDI_PROTECTMYSELF),
+	g_niData.hIcon = (HICON)::LoadImage(hInstance,MAKEINTRESOURCE(IDI_PROTECTMYSELF),
 		IMAGE_ICON, GetSystemMetrics(SM_CXSMICON),GetSystemMetrics(SM_CYSMICON),
 		LR_DEFAULTCOLOR);
 
 	// the window to send messages to and the message to send
 	//		note:	the message value should be in the
 	//				range of WM_APP through 0xBFFF
-	niData.hWnd = hWnd;
-	niData.uCallbackMessage = SWM_TRAYMSG;
+	g_niData.hWnd = hWnd;
+	g_niData.uCallbackMessage = PMS_TRAYMSG;
 
 	// tooltip message
-	lstrcpyn(niData.szTip, _T("Protect Yourself From Workstation."), sizeof(niData.szTip)/sizeof(TCHAR));
+	lstrcpyn(g_niData.szTip, _T("Protect Yourself From Workstation."), sizeof(g_niData.szTip)/sizeof(TCHAR));
 
-	Shell_NotifyIcon(NIM_ADD,&niData);
+	::Shell_NotifyIcon(NIM_ADD,&g_niData);
 
 	// free icon handle
-	if(niData.hIcon && DestroyIcon(niData.hIcon))
-		niData.hIcon = NULL;
+	if(g_niData.hIcon && ::DestroyIcon(g_niData.hIcon))
+		g_niData.hIcon = NULL;
+
+	// Default - Encable.
+	g_fDisabled = FALSE;
+	
+	// Register SessionNotification for catching Lock / UnLock time.
+	::WTSRegisterSessionNotification(hWnd, NOTIFY_FOR_ALL_SESSIONS);
+
+	g_nInterval = DEFAULT_INTERVAL;
+	Enable(hWnd);
 
 	// call ShowWindow here to make the dialog initially visible
-
- 	RECT rc = {0};
-	int nHeight, nLength = 0;
- 	if (::SystemParametersInfoW(SPI_GETWORKAREA, 0, &rc, 0))
-	{
- 		rc.left = (rc.left + rc.right) / 2;
- 		rc.top = (rc.top + rc.bottom) / 2;
-
-		nHeight = rc.top / 5;
-		nLength = rc.left / 5;
-
-		rc.left = rc.left - nLength / 2;
-		rc.top = rc.top - nHeight / 2;
- 	}
- 	::SetWindowPos(hWnd, NULL, rc.left, rc.top, nLength, nHeight, SWP_HIDEWINDOW);
-
-
-	fBlocked = FALSE;
-	::SetTimer(hWnd, IDC_TIMER,  50*60*1000, 0);
-	::WTSRegisterSessionNotification(hWnd, NOTIFY_FOR_ALL_SESSIONS);
-	
- 	::ShowWindow(hWnd, nCmdShow);
- 	::UpdateWindow(hWnd);
+	// Default = Hide;
 
 	return TRUE;
 }
 
-//
-//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  목적: 주 창의 메시지를 처리합니다.
-//
-//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
-//  WM_PAINT	- 주 창을 그립니다.
-//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+BOOL OnInitDialog(HWND hWnd)
+{
+
+	HICON hIcon = (HICON)::LoadImage(g_hInst,
+		MAKEINTRESOURCE(IDI_PROTECTMYSELF),
+		IMAGE_ICON, 0,0, LR_SHARED|LR_DEFAULTSIZE);
+	::SendMessage(hWnd,WM_SETICON,ICON_BIG,(LPARAM)hIcon);
+	::SendMessage(hWnd,WM_SETICON,ICON_SMALL,(LPARAM)hIcon);
+
+
+	return TRUE;
+}
+
+
+// Message handler for the app
+INT_PTR CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
 
-	switch (message)
+	switch (message) 
 	{
 	case WM_TIMER:
-		if(IDC_TIMER == wParam && FALSE == fBlocked)
+		if(IDC_TIMER == wParam && FALSE == g_fBlocked)
 		{
-			BlockInput(TRUE);
-			LockWorkStation();
+			::BlockInput(TRUE);
+			::LockWorkStation();
 		}
 		break;
 	case WM_WTSSESSION_CHANGE:
 		if(WTS_SESSION_LOCK == wParam)
 		{
-			fBlocked = TRUE;
-			KillTimer(hWnd, IDC_TIMER);
+			Disable(hWnd);
 		}
 		else if(WTS_SESSION_UNLOCK == wParam)
 		{
-			fBlocked = FALSE;
-			SetTimer(hWnd, IDC_TIMER, 50*60*1000, 0);
+			Enable(hWnd);
 		}
 		break;
-	case SWM_TRAYMSG:
+	case PMS_TRAYMSG:
 		switch(lParam)
 		{
 		case WM_LBUTTONDBLCLK:
-			ShowWindow(hWnd, SW_RESTORE);
+			::ShowWindow(hWnd, SW_RESTORE);
 			break;
 		case WM_RBUTTONDOWN:
 		case WM_CONTEXTMENU:
-			ShowContextMenu(hWnd);
+			::ShowContextMenu(hWnd);
 		}
 		break;
 	case WM_SYSCOMMAND:
-		if((wParam & 0xFFF0) == SC_MINIMIZE || (wParam & 0xFFF0) == SC_CLOSE )
+		if((wParam & 0xFFF0) == SC_MINIMIZE)
 		{
-			ShowWindow(hWnd, SW_HIDE);
+			::ShowWindow(hWnd, SW_HIDE);
 			return 1;
 		}
 		else if(wParam == IDM_ABOUT)
-			DialogBox(hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, (DLGPROC)About);
-		else
-			return DefWindowProc(hWnd, message, wParam, lParam);
+			::DialogBox(g_hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, (DLGPROC)About);
 		break;
 	case WM_COMMAND:
 		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// 메뉴 선택을 구문 분석합니다.
+		wmEvent = HIWORD(wParam); 
+
 		switch (wmId)
 		{
-		case SWM_SHOW:
-			ShowWindow(hWnd, SW_RESTORE);
+		case PMS_SHOW:
+			::ShowWindow(hWnd, SW_RESTORE);
 			break;
-		case SWM_HIDE:
-			ShowWindow(hWnd, SW_HIDE);
+		case PMS_HIDE:
+			::ShowWindow(hWnd, SW_HIDE);
+			break;
+		case IDOK:
+			if(g_fDisabled)
+			{
+				Enable(hWnd);
+			}
+			else
+			{
+				Disable(hWnd);
+			}
+
+			g_fDisabled = !g_fDisabled;
+			break;
+		case PMS_EXIT:
+			::DestroyWindow(hWnd);
 			break;
 		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			::DialogBox(g_hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, (DLGPROC)About);
 			break;
-		case IDM_EXIT:
-		case SWM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: 여기에 그리기 코드를 추가합니다.
-		EndPaint(hWnd, &ps);
+		return 1;
+	case WM_INITDIALOG:
+		return OnInitDialog(hWnd);
+	case WM_CLOSE:
+		::ShowWindow(hWnd, SW_HIDE);
 		break;
 	case WM_DESTROY:
-		KillTimer(hWnd, IDC_TIMER);
-		niData.uFlags = 0;
-		Shell_NotifyIcon(NIM_DELETE,&niData);
-		PostQuitMessage(0);
+		Disable(hWnd);
+		g_niData.uFlags = 0;
+		::Shell_NotifyIcon(NIM_DELETE,&g_niData);
+		::PostQuitMessage(0);
 		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
 
-// 정보 대화 상자의 메시지 처리기입니다.
+// About Dlg
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	UNREFERENCED_PARAMETER(lParam);
@@ -310,7 +269,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
 		{
-			EndDialog(hDlg, LOWORD(wParam));
+			::EndDialog(hDlg, LOWORD(wParam));
 			return (INT_PTR)TRUE;
 		}
 		break;
@@ -322,23 +281,23 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 void ShowContextMenu(HWND hWnd)
 {
 	POINT pt;
-	GetCursorPos(&pt);
-	HMENU hMenu = CreatePopupMenu();
+	::GetCursorPos(&pt);
+	HMENU hMenu = ::CreatePopupMenu();
 	if(hMenu)
 	{
-		if( IsWindowVisible(hWnd) )
-			InsertMenu(hMenu, -1, MF_BYPOSITION, SWM_HIDE, _T("Hide"));
+		if( ::IsWindowVisible(hWnd) )
+			::InsertMenu(hMenu, -1, MF_BYPOSITION, PMS_HIDE, _T("Hide"));
 		else
-			InsertMenu(hMenu, -1, MF_BYPOSITION, SWM_SHOW, _T("Show"));
-		InsertMenu(hMenu, -1, MF_BYPOSITION, SWM_EXIT, _T("Exit"));
+			::InsertMenu(hMenu, -1, MF_BYPOSITION, PMS_SHOW, _T("Show"));
+		::InsertMenu(hMenu, -1, MF_BYPOSITION, PMS_EXIT, _T("Exit"));
 
 		// note:	must set window to the foreground or the
 		//			menu won't disappear when it should
-		SetForegroundWindow(hWnd);
+		::SetForegroundWindow(hWnd);
 
-		TrackPopupMenu(hMenu, TPM_BOTTOMALIGN,
+		::TrackPopupMenu(hMenu, TPM_BOTTOMALIGN,
 			pt.x, pt.y, 0, hWnd, NULL );
-		DestroyMenu(hMenu);
+		::DestroyMenu(hMenu);
 	}
 }
 
@@ -347,11 +306,11 @@ ULONGLONG GetDllVersion(LPCTSTR lpszDllName)
 {
 	ULONGLONG ullVersion = 0;
 	HINSTANCE hinstDll;
-	hinstDll = LoadLibrary(lpszDllName);
+	hinstDll = ::LoadLibrary(lpszDllName);
 	if(hinstDll)
 	{
 		DLLGETVERSIONPROC pDllGetVersion;
-		pDllGetVersion = (DLLGETVERSIONPROC)GetProcAddress(hinstDll, "DllGetVersion");
+		pDllGetVersion = (DLLGETVERSIONPROC)::GetProcAddress(hinstDll, "DllGetVersion");
 		if(pDllGetVersion)
 		{
 			DLLVERSIONINFO dvi;
@@ -362,7 +321,54 @@ ULONGLONG GetDllVersion(LPCTSTR lpszDllName)
 			if(SUCCEEDED(hr))
 				ullVersion = MAKEDLLVERULL(dvi.dwMajorVersion, dvi.dwMinorVersion,0,0);
 		}
-		FreeLibrary(hinstDll);
+		::FreeLibrary(hinstDll);
 	}
 	return ullVersion;
+}
+
+
+void SetTimer(HWND hWnd, UINT nInterval)
+{
+	// SetTimer : Interval Time - Default 50 minutes;
+	g_fBlocked = FALSE;
+	::SetTimer(hWnd, IDC_TIMER,  nInterval, 0);
+}
+
+void KillTimer(HWND hWnd)
+{
+	g_fBlocked = TRUE;
+	::KillTimer(hWnd, IDC_TIMER);
+}
+
+void Disable(HWND hWnd)
+{
+	if(NULL != ::GetDlgItem(hWnd, IDOK))
+	{
+		::SetDlgItemTextW(hWnd, IDOK, L"ENABLE");
+	}
+	SetWindowTextW(hWnd, L"Disabled...");
+	KillTimer(hWnd);
+}
+
+void Enable(HWND hWnd)
+{
+	if(NULL != ::GetDlgItem(hWnd, IDOK))
+	{
+		::SetDlgItemTextW(hWnd, IDOK, L"DISABLE");
+
+		BOOL bChk = ::IsDlgButtonChecked(hWnd, IDC_INTERVAL);
+		if(bChk)
+		{
+			int nInterval = 1000 * 60 * ::GetDlgItemInt(hWnd, IDC_MINUTES,NULL, FALSE); 
+			if(nInterval != g_nInterval)
+			{
+				::MessageBoxW(hWnd, L"You change the interval time.", L"Alert", 0);
+			}
+			g_nInterval = nInterval;
+		}
+		else
+			g_nInterval = DEFAULT_INTERVAL;
+	}
+	SetWindowTextW(hWnd, L"Now Running...");
+	SetTimer(hWnd, g_nInterval);
 }
--- ProtectMySelf_ForWindows/ProtectMySelf/stdafx.h
+++ ProtectMySelf_ForWindows/ProtectMySelf/stdafx.h
@@ -24,3 +24,5 @@
 
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+
+#pragma comment(lib,"wtsapi32.lib")
